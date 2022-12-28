1e61d574bc91d20e1eca204ea596d4766f9c6f73
hiepnguyen31896 <111103059+hiepnguyen31896@users.noreply.github.com>
2022-12-28 13:11:57 +0700

    admin update

--- src/main/java/com/javateam/mgep/controller/AdminController.java
+++ src/main/java/com/javateam/mgep/controller/AdminController.java
@@ -1,12 +1,39 @@
 package com.javateam.mgep.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.javateam.mgep.entity.CustomUserDetails;
+import com.javateam.mgep.entity.Employee;
+import com.javateam.mgep.service.AdminService;
 
 @Controller
 public class AdminController {
+	
+	@Autowired
+	AdminService adminService;
+	
     @GetMapping("/admin/home")
-    public String HomeAdmin(){
+    public String HomeAdmin(Model model, HttpSession session, String keyword){
+    	System.out.println(keyword);
+    	if(keyword == null) {
+    		List<Employee> list = adminService.searchEmployee("");
+    		model.addAttribute("list",list);
+    	}
+    	else {
+    		List<Employee> list = adminService.searchEmployee(keyword);
+    		model.addAttribute("list",list);
+    	}
         return "/admin/home";
     }
+    
+    
 }

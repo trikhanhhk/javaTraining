e99b77a096c949c2fb1c27335e4f9a2bded2825f
hiepnguyen31896 <111103059+hiepnguyen31896@users.noreply.github.com>
2022-12-30 15:32:52 +0700

    Add files via upload

--- src/main/java/com/javateam/mgep/service/AdminService.java
+++ src/main/java/com/javateam/mgep/service/AdminService.java
@@ -1,5 +1,7 @@
 package com.javateam.mgep.service;
 
+import java.io.Writer;
+import java.text.ParseException;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -9,6 +11,10 @@ import com.javateam.mgep.entity.Employee;
 public interface AdminService {
 	public List<Employee> searchEmployee(String email);
 	
-	public void sendEmail(String email, String title, String content, boolean interval, String value );
+	public void sendEmail(String email, String title, String content, String interval, String value );
+	
+	public void updateAdmin(Long id, String email, String firstName, String lastName, String dateOfBirth, String phoneNumber, String address) throws ParseException;
+	
+	public void exportAdmin(Writer writer);
 
 }

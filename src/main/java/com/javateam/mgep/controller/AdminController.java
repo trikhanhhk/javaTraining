b1d97f37b91a0d7eed166e18e90e9d2aff80c2f8
trikhanhhk <nguyentrikhanh123hk@gmail.com>
2023-01-17 14:32:33 +0700

    ten file export

--- src/main/java/com/javateam/mgep/controller/AdminController.java
+++ src/main/java/com/javateam/mgep/controller/AdminController.java
@@ -99,11 +99,11 @@ public class AdminController {
     @GetMapping("/admin/export-to-excel")
     public void exportIntoExcelFile(HttpServletResponse response) throws IOException {
         response.setContentType("application/octet-stream");
-        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        DateFormat dateFormatter = new SimpleDateFormat("dd-MM-yyyy_HH:mm:ss");
         String currentDateTime = dateFormatter.format(new Date());
 
         String headerKey = "Content-Disposition";
-        String headerValue = "attachment; filename=DS_NV" + currentDateTime + ".xlsx";
+        String headerValue = "attachment; filename=DS_NV_" + currentDateTime + ".xlsx";
         response.setHeader(headerKey, headerValue);
 
         List<Employee> listOfStudents = employeeService.getListAll();
